45b38092b4635f40b5ab2ad08787e81410aa9f4a
Gui Reis <gui.sreis25@gmail.com>
2026-04-14 00:06:42 -0300

    My Solution

--- Exercises/HackerRank/Swift/Application/Sources/main.swift
+++ Exercises/HackerRank/Swift/Application/Sources/main.swift
@@ -1,3 +1,6 @@
 // Entry point
-let meetings = [[1, 3], [1, 2], [2, 3], [3, 4]]
-print(meetings.sorted { $0[0] < $1[0] })
+//let meetings = [[1, 3], [1, 2], [2, 3], [3, 4]]
+//print(meetings.sorted { $0[0] < $1[0] })
+
+
+print(1+2+3+4+5+6+7+8+9)
